fe637d46d0d7ae440e003645e4c80a132b719ec8
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-06 13:57:56 +0530

    routes: update routes and create consultation route && database changes

--- src/routes/BookingRoute.ts
+++ src/routes/BookingRoute.ts
@@ -1,52 +1,184 @@
 import { Router } from "express";
 import fileUpload, { UploadedFile } from "express-fileupload";
+import { checkSchema, validationResult } from "express-validator";
 import FormData from "form-data";
 import fetch from "node-fetch";
 import prisma from "../lib/prisma";
 
 const router = Router();
-router.use();
 router.use(fileUpload());
 
-router.post("/test", async (req, res) => {
-  if (
-    !req.files ||
-    Object.keys(req.files).length === 0 ||
-    !req.files.prescription_file
-  ) {
-    res.status(400).json({ message: "prescription image not provided" });
-    return;
+router.post(
+  "/test",
+  checkSchema({
+    first_name: {
+      isString: true,
+    },
+    last_name: {
+      isString: true,
+    },
+    hospital_name: {
+      isString: true,
+    },
+    email: {
+      custom: {
+        options: (value, { req }) => {
+          prisma.users.findUnique({ where: { email: value } }).then((test) => {
+            if (!test) {
+              return Promise.reject("User not found");
+            }
+          });
+        },
+      },
+    },
+    prescriptionDate: {
+      isString: true,
+    },
+  }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    if (
+      !req.files ||
+      Object.keys(req.files).length === 0 ||
+      !req.files.prescription_file
+    ) {
+      res.status(400).json({ message: "prescription image not provided" });
+      return;
+    }
+
+    const { first_name, last_name, email, hospital_name, prescription_date } =
+      req.body;
+
+    const prescriptionImage: UploadedFile = req.files
+      .prescription_file as UploadedFile;
+    const formData = new FormData();
+    formData.append("image", prescriptionImage.data, {
+      filename: prescriptionImage.name,
+    });
+
+    try {
+      const imgurResponse = await fetch("https://api.imgur.com/3/upload", {
+        method: "POST",
+        body: formData,
+      }).then((requestResponse) => requestResponse.json());
+
+      const booking = await prisma.test.create({
+        data: {
+          prescription_image: imgurResponse.data.link,
+          user_email: email,
+          first_name,
+          last_name,
+          hospital_name,
+          prescription_date,
+        },
+      });
+
+      res.json(booking);
+    } catch (error) {
+      console.error(error);
+    }
   }
+);
 
-  const { first_name, last_name, hospital_name, prescription_date } = req.body;
+router.post(
+  "/consult",
+  checkSchema({
+    first_name: {
+      isString: true,
+    },
+    last_name: {
+      isString: true,
+    },
+    hospital_name: {
+      isString: true,
+    },
+    city: {
+      isString: true,
+    },
+    health_condition: {
+      isString: true,
+      custom: {
+        options: (value, { req }) => {
+          if (!["low", "mid", "severe"].includes(value.toLowerCase())) {
+            return Promise.reject("health condition not found");
+          }
+        },
+      },
+    },
+    email: {
+      custom: {
+        options: (value, { req }) => {
+          prisma.users.findUnique({ where: { email: value } }).then((test) => {
+            if (!test) {
+              return Promise.reject("User not found");
+            }
+          });
+        },
+      },
+    },
+    prescriptionDate: {
+      isString: true,
+    },
+  }),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+      }
 
-  const prescriptionImage: UploadedFile = req.files
-    .prescription_file as UploadedFile;
-  const formData = new FormData();
-  formData.append("image", prescriptionImage.data, {
-    filename: prescriptionImage.name,
-  });
+      if (
+        !req.files ||
+        Object.keys(req.files).length === 0 ||
+        !req.files.prescription_file
+      ) {
+        res.status(400).json({ message: "prescription image not provided" });
+        return;
+      }
 
-  try {
-    const imgurResponse = await fetch("https://api.imgur.com/3/upload", {
-      method: "POST",
-      body: formData,
-    }).then((requestResponse) => requestResponse.json());
+      const {
+        first_name,
+        last_name,
+        email,
+        hospital_name,
+        city,
+        health_condition,
+        prescription_date,
+      } = req.body;
 
-    const booking = await prisma.service.create({
-      data: {
+      const prescriptionImage: UploadedFile = req.files
+        .prescription_file as UploadedFile;
+      const formData = new FormData();
+      formData.append("image", prescriptionImage.data, {
+        filename: prescriptionImage.name,
+      });
+
+      const imgurResponse = await fetch("https://api.imgur.com/3/upload", {
+        method: "POST",
+        body: formData,
+      }).then((requestResponse) => requestResponse.json());
+
+      await prisma.consult.create({
         prescription_image: imgurResponse.data.link,
+        use_email: email,
         first_name,
         last_name,
         hospital_name,
+        city,
+        health_condition,
         prescription_date,
-      },
-    });
-
-    res.json(booking);
-  } catch (error) {
-    console.error(error);
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
   }
-});
+);
 
 export default router;
--- src/routes/LabRoute.ts
+++ src/routes/LabRoute.ts
@@ -4,23 +4,23 @@ import prisma from "../lib/prisma";
 const router = Router();
 
 router.post("/register", async (req, res) => {
-    try {
-        const { first_name, last_name, name, password, city } = req.body;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const service = await prisma.service.create({
-            data: {
-                password: hashedPassword,
-                first_name,
-                last_name,
-                name,
-                city,
-            },
-        });
-        res.json(service);
-    } catch (error) {
-        res.status(500).json({ error });
-    }
+  try {
+    const { first_name, last_name, name, password, city } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const service = await prisma.lab.create({
+      data: {
+        password: hashedPassword,
+        first_name,
+        last_name,
+        name,
+        city,
+      },
+    });
+    res.json(service);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 export default router;
