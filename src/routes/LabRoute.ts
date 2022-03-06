c29e798ff9e304e40f6600fd7faed1dd86a551bb
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-06 12:25:54 +0530

    lab: hash password

--- src/routes/LabRoute.ts
+++ src/routes/LabRoute.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
+import bcrypt from "bcrypt"
 import prisma from "../lib/prisma";
 const router = Router();
 
 router.post("/register", async (req, res) => {
     try {
-        const { first_name, last_name, name, image, city } = req.body;
+        const { first_name, last_name, name, password, image, city } = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const service = await prisma.service.create({
             data: {
+                password: hashedPassword,
                 first_name,
                 last_name,
                 name,
